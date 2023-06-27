0f91f835b232fb4bfa27ab24ba3d15b8ca0265f1
AliI Raza <aliraza6136@gmail.com>
2023-06-27 14:20:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import json
 from flask import Flask, request, jsonify
 
-import gpt4free
-from gpt4free import Provider
 
 app = Flask(__name__)
 @app.route('/')
 def chatapi():
-    query = request.args.get('q')
-    response = gpt4free.Completion.create(Provider.You, query)
+    response="hello"
     return response
 if __name__ == '__main__':
     app.run(debug=True)
