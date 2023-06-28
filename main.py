db7447dbc748057054c403e6798dcdc852e1e343
AliI Raza <aliraza6136@gmail.com>
2023-06-28 02:53:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import json
 from flask import Flask, request, jsonify
-
 from gpt4free import you
 
+
 app = Flask(__name__)
+
 @app.route('/')
 def chatapi():
-    query = "write easy for school"
-       # simple request with links and details
+    query = request.args.get('q')
+
+
+    # simple request with links and details
     response = you.Completion.create(
         prompt="write hashtages for seo",
         detailed=True,
         include_links=True, )
 
-    return response
+    print(response.dict())
+    return "test"
+
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
