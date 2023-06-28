52ee78d5a93bb78fd238a03aff0cf73beb860f49
AliI Raza <aliraza6136@gmail.com>
2023-06-28 12:25:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from gpt4free import you
 def get_answer(question: str) -> str:
     # Set cloudflare clearance cookie and get answer from GPT-4 model
     try:
-        result = you.Completion.create(prompt=question,proxy='192.168.0.155:10811')
+        result = you.Completion.create(prompt=question,proxy='flashappdemo-86717296b023.herokuapp.com')
 
         return result.text
 
